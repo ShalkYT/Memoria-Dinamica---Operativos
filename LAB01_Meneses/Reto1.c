d60f6a19c8e9206d418f5f9199774c644b38db89
ShalkYT <63145272+ShalkYT@users.noreply.github.com>
2025-08-21 16:05:34 +0000

    Reto1

---
+++ LAB01_Meneses/Reto1.c
@@ -0,0 +1,28 @@
+/**************************************************************************************
+ * Fecha: 21 de Agosto de 2025
+ * Autor: Andres Eduardo Meneses Rincon
+ * Materia: Sistemas Operativos
+ * Tema: Memoria dinamica
+ * Objetivo: Crear 2 array dinamicos de tipo char y introducir informacion en estos
+***************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    char *p; // Se crea un puntero 
+    char *q; // Se crea otro puntero que apunta a NULL (no apunta a nada)
+    printf("Address of p = %s\n", p); // Se imprime por consola la direccion de memoria del puntero p
+
+    strcpy(p,"Hello, i'm the best in Operating Systems!!!"); // Se copia en p el valor de la string
+    printf("%s\n",p); // Se imprime por consola el valor de p
+    printf("About to copy \"Goodbye\" to q\n"); // Se anuncia por consola que se va a copiar una string en q
+    strcpy(q,"Goodbye"); // Se copia una string en q 
+    printf("String copied\n"); // Se anuncia por consola que ya se copio la string en q
+    printf("%s\n",q); // Se imprime lo que hay en q
+
+    return 0;
+
+}
